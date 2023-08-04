34a3ca868acbb7a5d288803ea491860417fe11cc
Sirothpech <christophe.ngan@gmail.com>
2023-08-04 18:07:03 +0200

    Update dbstorage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
 import pycodestyle
 import unittest
-import console
-from console import HBNBCommand
+from models.engine.db_storage import DBStorage
 
 
-class test_console(unittest.TestCase):
+class test_db_storage(unittest.TestCase):
     """Test class for db storage"""
 
-    def test_doc(self):
-        """Tests the documentation"""
-
-        self.assertGreater(len(console.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_all.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_create.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_destroy.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_quit.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_EOF.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_count.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_destroy.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.do_update.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.emptyline.__doc__), 0)
-
-        self.assertGreater(len(HBNBCommand.default.__doc__), 0)
-
-    def test_pep8_console(self):
+    def test_pep8_DBStorage(self):
         """pep8 test"""
         style = pycodestyle.StyleGuide(quiet=True)
-        p = style.check_files(['console.py'])
+        p = style.check_files(['models/engine/db_storage.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
+    def test_documentation_module(self):
+        """Tests the the documentation of the module"""
+        self.assertGreater(len(DBStorage.__doc__), 0)
+
+    def test_documentation_class(self):
+        """Tests the the documentation of the class"""
+        self.assertGreater(len(DBStorage.__doc__), 0)
+
+    def test_documentation_init(self):
+        """Tests the the documentation of init"""
+        self.assertGreater(len(DBStorage.__init__.__doc__), 0)
+
+    def test_documentation_all(self):
+        """Tests the the documentation of all"""
+        self.assertGreater(len(DBStorage.all.__doc__), 0)
+
+    def test_documentation_new(self):
+        """Tests the the documentation of new"""
+        self.assertGreater(len(DBStorage.new.__doc__), 0)
+
+    def test_documentation_save(self):
+        """Tests the the documentation of save"""
+        self.assertGreater(len(DBStorage.save.__doc__), 0)
+
+    def test_documentation_delete(self):
+        """Tests the the documentation of delete"""
+        self.assertGreater(len(DBStorage.delete.__doc__), 0)
+
+    def test_documentation_reload(self):
+        """Tests the the documentation of reload"""
+        self.assertGreater(len(DBStorage.reload.__doc__), 0)
