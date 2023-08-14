9cb217db81b701f4b1a3ee2b4ea3b053ad2ecbda
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-14 13:24:39 +0200

    Add: task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,29 +7,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
-  """
-  Route handler for the root URL ("/").
+    """
+    Route handler for the root URL ("/").
 
-  This function is executed when the root URL ("/") is accessed.
+    This function is executed when the root URL ("/") is accessed.
 
-  Returns:
-      str: A simple greeting message.
-  """
-  return("Hello HBNB!")
+    Returns:
+        str: A simple greeting message.
+    """
+    return ("Hello HBNB!")
+
 
 @app.route("/hbnb")
 def message():
-  """
-  Route handler for the root URL ("/hbnb").
+    """
+    Route handler for the root URL ("/hbnb").
 
-  This function is executed when the root URL ("/hbnb") is accessed.
+    This function is executed when the root URL ("/hbnb") is accessed.
 
-  Returns:
-      str: "HBNB".
-  """
-  return("HBNB")
+    Returns:
+        str: "HBNB".
+    """
+    return ("HBNB")
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
