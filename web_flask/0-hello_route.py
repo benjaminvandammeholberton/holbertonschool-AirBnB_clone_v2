c20960bd2ecc57363117e3ea5dc4c9d81f8475f0
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-14 13:16:34 +0200

    Fix task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,17 +6,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
-  """
-  Route handler for the root URL ("/").
+    """
+    Route handler for the root URL ("/").
 
-  This function is executed when the root URL ("/") is accessed.
-  
-  Returns:
-      str: A simple greeting message.
-  """
-  return("Hello HBNB!")
+    This function is executed when the root URL ("/") is accessed.
+
+    Returns:
+        str: A simple greeting message.
+    """
+    return("Hello HBNB!")
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
