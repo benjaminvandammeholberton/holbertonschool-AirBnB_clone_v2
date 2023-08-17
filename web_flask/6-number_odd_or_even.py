862abe97a45f8b8b604a27619cac19100f545905
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-17 13:09:40 +0200

    Add: task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -110,7 +110,8 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def odd_or_even(n):
     number_type = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, number_type=number_type)
+    return render_template('6-number_odd_or_even.html',
+                           n=n, number_type=number_type)
 
 
 if __name__ == '__main__':
